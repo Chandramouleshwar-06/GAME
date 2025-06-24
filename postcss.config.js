f0a5ec11770b4627001fc0cbfa06919cb522fdde
Chandramouleshwar-06 <crchandramouleshwar03@gmail.com>
2025-06-24 21:32:05 +0530

    Updated postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
-    '@tailwindcss/postcss': {}, // Updated: Use the new plugin name
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   }
 }
-=======
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
->>>>>>> 427e00c43f05f175a55dde03cd6b245728f60d31
